a59e6079235c792b98c11422856ebeed03a05bba
guoxiaoxiao <1242512811@qq.com>
2019-09-19 10:51:16 +0800

    1.1 紧急任务版本推送到远程仓库

--- src/main/java/com/pojo/DeptPojo.java
+++ src/main/java/com/pojo/DeptPojo.java
@@ -1,6 +1,12 @@
 package com.pojo;
 
 public class DeptPojo {
+    /**
+     * 部门名称
+     */
     private String name;
-    
+    /***
+     * 部门电话
+     */
+    private String phone;
 }
---
+++ src/main/java/com/pojo/Item.java
@@ -0,0 +1,12 @@
+package com.pojo;
+
+/**
+ * 商品
+ */
+public class Item {
+
+    /**
+     * 商品名称
+     */
+    private String name;
+}
